18b8631ef65cd93ec684a54ac36c17acbdad4a31
UsmanGhani552 <usman.centosquare@gmail.com>
2025-10-08 14:27:18 +0500

    attendence seeder bug fixes

--- database/seeders/AttendenceSeeder.php
+++ database/seeders/AttendenceSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Attendence;
 use App\Models\User;
 use App\Services\ZKTecoService;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -22,23 +23,27 @@ class AttendenceSeeder extends Seeder
     public function run(): void
     {
         $attendences = $this->zkService->getAttendance();
-        
+
         if ($attendences) {
             foreach ($attendences as $attendence) {
                 $user = User::where('id', $attendence['user_id'])->first();
                 if (!$user) {
                     continue; // Skip if user not found
                 }
+                $punchDateTime = Carbon::parse($attendence['timestamp']);
                 $punchTime = date('H:i:s', strtotime($attendence['timestamp']));
                 $checkInFrom = $user->shift->check_in_from; // "17:00:00"  // "08:00:00"
                 $checkInTo = $user->shift->check_in_to;     // "03:00:00"  // "17:00:00"
 
-                $type = ($checkInFrom < $checkInTo)? (($punchTime >= $checkInFrom && $punchTime <= $checkInTo) ? 'check in' : 'check out'):
-                (($punchTime >= $checkInFrom || $punchTime <= $checkInTo) ? 'check in' : 'check out');
+                $type = ($checkInFrom < $checkInTo) ? (($punchTime >= $checkInFrom && $punchTime <= $checkInTo) ? 'check in' : 'check out') : (($punchTime >= $checkInFrom || $punchTime <= $checkInTo) ? 'check in' : 'check out');
+                // ✅ Handle after-midnight punches (belong to previous day)
+                if ($punchTime < '09:30:00') {
+                    $punchDateTime->subDay();
+                }
                 $attendence = Attendence::updateOrCreate(
                     [
                         'user_id' => $user->id,
-                        'timestamp' => $attendence['timestamp'],
+                        'timestamp' => $punchDateTime,
                     ],
                     [
                         'type' => $type,
